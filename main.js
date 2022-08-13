4b71bd03a9e8c2bd0d41ea9d97116b182b6c5934
hodu31 <101084688+hodu31@users.noreply.github.com>
2022-08-13 15:42:54 +0900

    JS : Home transparent

--- main.js
+++ main.js
@@ -29,7 +29,17 @@ homeContactBtn.addEventListener('click', () => {
     scrollIntoView('#contact')
 })
 
+// Make home slowly fade to transparent as the window scrolls down
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+    home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+
+
 function scrollIntoView(selector) {
     const scrollTO = document.querySelector(selector);
     scrollTO.scrollIntoView({behavior: "smooth"});
 }
+
