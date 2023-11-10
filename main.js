61b88129405b203d4ad1a4221b06d92943723aee
jeonsungsam <sungsam9709@gmail.com>
2023-11-10 19:57:29 +0900

    롤링 페이퍼

--- main.js
+++ main.js
@@ -89,3 +89,16 @@ function scrollIntoView(selector) {
     scrollTO.scrollIntoView({behavior: "smooth"});
 }
 
+document.getElementById('messageForm').addEventListener('submit', function(event){
+    event.preventDefault();
+
+    var message = document.getElementById('messageInput').value.trim();
+    if(message) {
+        var messagesContainer = document.getElementById('messages');
+        var newMessage = document.createElement('p');
+        newMessage.textContent = message;
+        messagesContainer.appendChild(newMessage);
+
+        document.getElementById('messageInput').value = ''; // 입력 필드 초기화
+    }
+});
